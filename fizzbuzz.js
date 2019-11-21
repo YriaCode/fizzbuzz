60debc754ec737cee93971c0153a0ce3520922ea
Dan-iel36 <danielbediako36@gmail.com>
2019-11-21 16:33:20 +0100

    finish

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,22 +1,18 @@
 
 function fizzbuzz(i){
 	let r
+	if (i % 5 ==0 && i % 3 ==0){
+		return "fizzbuzz";
+	}
 	 if (i % 5 == 0){
-		r = "buzz";
+		return "buzz";
 	}
 	 if (i % 3 == 0){
-		r = "fizz";
-	}
-	if (i % 5 ==0 && i % 3 ==0){
-		r = "fizzbuzz";
+		return "fizz";
 	}
 
 	return i;
-}
+	}
 for(let i = 1; i< 101; i++){
 	console.log(fizzbuzz(i));
 }
-
-
-
-
